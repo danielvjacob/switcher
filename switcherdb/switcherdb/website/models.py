9454cd7d93373c05e78397afb6088f7ca9ba42a2
danielvjacob <98068637+danielvjacob@users.noreply.github.com>
2022-06-28 15:05:27 -0500

    added multipage form

--- switcherdb/switcherdb/website/models.py
+++ switcherdb/switcherdb/website/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from multipage_form.models import MultipageModel
 
-class User(models.Model):
-    username = models.EmailField(max_length=200)
-    password = models.CharField(max_length=200)
-    credit_num = models.IntegerField(default=0)
-    expiration = models.IntegerField(default=0000)
-    cvc = models.IntegerField(default=000)
-    card_type = models.CharField(max_length=200, default="None")
+class User(MultipageModel):
+    username = models.EmailField(max_length=200, blank=True)
+    password = models.CharField(max_length=200, blank=True)
+    credit_num = models.IntegerField(default=0, blank=True)
+    expiration = models.IntegerField(default=0000, blank=True)
+    cvc = models.IntegerField(default=000, blank=True)
+    card_type = models.CharField(max_length=200, default="None", blank=True)
 
     def __str__(self):
         return self.username
