1d6cd0c6804255f45f12ad8489997eef6b28fd72
Derek Huang <derekh9896@gmail.com>
2022-06-28 15:47:28 -0400

    test commit

--- switcherdb/switcherdb/website/admin.py
+++ switcherdb/switcherdb/website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Billing_info, User, Service
+from .models import User, Service
 
 admin.site.register(User)
 admin.site.register(Service)
--- switcherdb/switcherdb/website/models.py
+++ switcherdb/switcherdb/website/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class User(models.Model):
     username = models.EmailField(max_length=200)
     password = models.CharField(max_length=200)
-    credit_num = models.IntegerField()
-    expiration = models.IntegerField()
-    cvc = models.IntegerField()
-    card_type = models.CharField(max_length=200)
+    credit_num = models.IntegerField(default=0)
+    expiration = models.IntegerField(default=0000)
+    cvc = models.IntegerField(default=000)
+    card_type = models.CharField(max_length=200, default="None")
 
     def __str__(self):
         return self.username
